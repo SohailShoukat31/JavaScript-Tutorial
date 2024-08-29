eef5ddf483c5d66a10b1f04e7b3c7d0831a4ca85
Sohail Shoukat <sohailshoukat131@gmail.com>
2024-08-29 22:39:51 +0500

    Learn Js

---
+++ 02-Alerts/index.js
@@ -0,0 +1,11 @@
+// An alert is a box that pops up to give the user a message. Here's code for an alert that
+// displays the message "Thanks for your input!
+
+alert("Thanks for your Input");
+// alert is a keyword
+
+// Some coders write window.alert instead of, simply, alert. This is a highly formal but
+// perfectly correct way to write it. Most coders prefer the short form. We'll stick to the
+// short form in this training.
+
+window.alert("Thanks for your Input");
