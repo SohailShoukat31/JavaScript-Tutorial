255a1dfb96f32a910b7b6441ad352cd4718243b4
Sohail Shoukat <sohailshoukat131@gmail.com>
2024-09-11 09:40:49 +0500

    👉Update

---
+++ 14_Arrays/index.js
@@ -0,0 +1,21 @@
+var city0 = "Karachi";
+var city1 = "Lahore";
+var city2 = "Quetta";
+var city3 = "Peshawar";
+var city4 = "Islamabad";
+alert("Welcome to " + city4);
+
+//  I'm talking about a type of variable called an array , . Whereas an ordinary variable has a single value assigned to it—
+var cities = ["Karachi", "Lahore", "Quetta", " Peshawar ", "Islamabad"];
+console.log(cities);
+console.log(cities[1]); // The first element in the list always has an index of 0, the second element an index of 1, and so on.
+// alert("Welcome to " + cities[0]);
+
+// An array can be assigned any type of value that you can assign to ordinary variables.
+var mixArray = ["String", 12345, true];
+console.log(mixArray);
+
+// example
+
+var fruitsName = ["Apple", "Banana", "Pineapple", "Grapes"];
+console.log(fruitsName);
---
+++ 15_Adding and removing elements/index.js
@@ -0,0 +1,10 @@
+var animalsName = ["Dog", "Cat", "Snake", "Lizard"];
+//1  .push()
+// Adds one or more elements to the end of an array and returns the new length of the array.
+animalsName.push("Goat");
+console.log(animalsName);
+
+// 2 .pop()
+// Removes the last element from an array and returns that element.
+animalsName.pop();
+console.log(animalsName);   // Remove goat
