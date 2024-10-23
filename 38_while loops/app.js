08b82d42542c724a6af1a29fcaccf24d9747ec9d
Sohail Shoukat <sohailshoukat131@gmail.com>
2024-10-23 19:59:34 +0500

    👉Update

---
+++ 38_while loops/app.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+var i = 0;
+while (i < 11) {
+  console.log(i);
+  i++;
+}
+
+var count = 1; // Initialize a variable
+while (count <= 5) {
+  // Condition to check
+  console.log(count);
+  count++; // Increment the count
+}
+
+var j = 1;
+do {
+  console.log(j);
+  j++;
+} while (j <= 5);
