51f9a9d1cd572c6e19d3afb37084796528ae87c6
Sohail Shoukat <sohailshoukat131@gmail.com>
2024-10-24 17:40:42 +0500

    👉Update

---
+++ 39_For,While & do While Loops/index.js
@@ -0,0 +1,15 @@
+document.write("Hello World")
+
+
+// while(condition){
+
+// }
+
+var index = 0;
+while(index <= 10){
+    console.log(`Value of index of ${index}`)
+    index = index + 2
+
+}
+
+var newArray = ["Apple" , "Banana" , "Carrot"];
