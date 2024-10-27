038e3350a63cd3b7b0d1cda6e5131614f78f055b
Sohail Shoukat <sohailshoukat131@gmail.com>
2024-10-27 15:56:03 +0500

    👉Update

---
+++ 41_Reading field values/app.js
@@ -0,0 +1,7 @@
+// document.write("Hello World !")
+
+ function checkAddress(fieldId) {
+     if (document.getElementById(fieldId).value === "") {
+     alert("Email address required.");
+     }
+     }
