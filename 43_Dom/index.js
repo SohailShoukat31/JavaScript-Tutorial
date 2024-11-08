80798ae5ceb50b8537b2f49ba4562c8a8a8713e9
Sohail Shoukat <sohailshoukat131@gmail.com>
2024-11-08 11:06:08 +0500

    👉Update

---
+++ 43_Dom/index.js
@@ -0,0 +1,9 @@
+// The DOM (Document Object Model)  is a programming interface for web documents
+//  It represents the structure of a web page as a tree of objects,
+// where each object corresponds to a part of the page (like elements, attributes, and text).
+//JavaScript can interact with the DOM to dynamically modify the content and structure of a webpage.
+
+console.log("Hello World");
+window.console.log("Hello ");
+console.log(window);
+console.log(window.document);
