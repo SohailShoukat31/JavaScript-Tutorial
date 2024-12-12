c28bde07e3b59cd0edccd5fa6e2a0113918abb7a
Sohail Shoukat <sohailshoukat131@gmail.com>
2024-12-12 18:59:59 +0500

    👉update

---
+++ 58-Dom (ongoing)/mycode.js
@@ -0,0 +1,17 @@
+// console.log("I love my Code ");
+// window.console.log("I love my Code");
+
+
+// Accessing the Window object properties
+console.log(window.innerWidth);
+console.log(window.innerHeight);
+
+
+// Using Window object methods
+
+// window.alert("This is an alert");
+
+// Alert Boxes: A common example of the window object in action is showing an alert box. Suppose you're visiting an e-commerce website, and the website needs to notify you of a new sale. It might use window.alert("New Sale! 50% off on all items!") to display a message to you.
+// window.alert("New Sale! 50% off on all items!")
+
+
---
+++ 58-Dom1 (ongoing)-/index.js
@@ -0,0 +1,21 @@
+//  Get Element by Id
+
+var myId = document.getElementById("main-heading");
+console.dir(myId);
+var btn = document.getElementById("btn");
+console.dir(btn);
+
+// Get Element by Class .
+
+var Para = document.getElementsByClassName("my-text");
+console.dir(Para);
+
+// Get Element by Tag Name
+var myTag = document.getElementsByTagName("p");
+console.dir(myTag);
+
+// Query Slector
+
+// it return first found element
+//  var QuerySelector = document.querySelector("main-heading");
+//  console.dir(QuerySelector);
